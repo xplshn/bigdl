25a02e032df9ace5278ca2ee8c5b0db96b5bba22
xplshn <musicslave66@murena.io>
2024-05-24 21:04:18 -0300

    Fix info.go

--- info.go
+++ info.go
@@ -20,17 +20,26 @@ type BinaryInfo struct {
 
 func findBinaryInfo(metadata []map[string]interface{}, binaryName string) (BinaryInfo, bool) {
 	for _, binMap := range metadata {
-		if binMap["name"].(string) == binaryName {
+		if name, ok := binMap["name"].(string); ok && name == binaryName {
+			description, _ := binMap["description"].(string)
+			repo, _ := binMap["repo_url"].(string)
+			build_date, _ := binMap["build_date"].(string)
+			version, _ := binMap["repo_version"].(string)
+			updated, _ := binMap["repo_updated"].(string)
+			size, _ := binMap["size"].(string)
+			sha256, _ := binMap["sha256"].(string)
+			source, _ := binMap["download_url"].(string)
+
 			return BinaryInfo{
-				Name:        binMap["name"].(string),
-				Description: binMap["description"].(string),
-				Repo:        binMap["repo_url"].(string),
-				ModTime:     binMap["build_date"].(string),
-				Version:     binMap["repo_version"].(string),
-				Updated:     binMap["repo_updated"].(string),
-				Size:        binMap["size"].(string),
-				SHA256:      binMap["sha256"].(string),
-				Source:      binMap["download_url"].(string),
+				Name:        name,
+				Description: description,
+				Repo:        repo,
+				ModTime:     build_date,
+				Version:     version,
+				Updated:     updated,
+				Size:        size,
+				SHA256:      sha256,
+				Source:      source,
 			}, true
 		}
 	}
@@ -45,7 +54,7 @@ func getBinaryInfo(binaryName string) (*BinaryInfo, error) {
 
 	binInfo, found := findBinaryInfo(metadata, binaryName)
 	if !found {
-		return nil, fmt.Errorf("info for the requested binary ('%s') not found in the metadata.json file", binaryName)
+		return nil, fmt.Errorf("error: info for the requested binary ('%s') not found in the metadata.json file", binaryName)
 	}
 
 	return &binInfo, nil
